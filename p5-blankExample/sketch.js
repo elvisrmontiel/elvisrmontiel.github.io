86acf21a18eb6f481e236dfb37bbfde593714cd4
elvisrmontiel <35942459+elvisrmontiel@users.noreply.github.com>
2018-03-08 11:48:09 -0800

    more p5 messing

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,17 +1,16 @@
-
-// declare variables here
-
-
-// setup runs once
 function setup() {
-	createCanvas(500, 500);
-	background(150);
+	createCanvas(600, 800);
+
+	background(200, 50, 50);
+
+	ellipse(width/2, height/2, 300, 200);
+	strokeWeight(10);
+
+	line(width/2-20, height/2-20, width/2-20, height/2-20);
+line(width/2+20, height/2+20, width/2-20, height/2-20);
+
+
+
+
+
 }
-
-// draw loops and loops
-function draw() {
-
-}
-
-
-// write functions here
